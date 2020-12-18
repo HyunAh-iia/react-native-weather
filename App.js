5c73d29bc072a0f54392db4eb80f0494d798369b
iia <kha2464@gmail.com>
2020-12-18 23:45:46 +0900

    [기능] 날씨정보 조회 추가
    - Axios 의존성 추가
    - 날씨 정보 API 호출

--- App.js
+++ App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import {Alert} from 'react-native'
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios'
+import API_KEY from './API_KEY.json'
+
+const APPID = API_KEY.openWeather; // https://openweathermap.org/ 로그인해서 API Key 넣으면 됨
 
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?appid=${APPID}&lat=${latitude}&lon=${longitude}`;
+    const { data } = await axios.get(url);
+    console.log(data);
   }
 
   getLocation = async() => {
@@ -16,6 +26,8 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude }
       } = location;
+
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (e) {
       Alert.alert("위치를 찾지 못하였습니다.", "흑흑");
