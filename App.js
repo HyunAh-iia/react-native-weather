98a3927149b37bb1327fe8f619ca31a88b9a0055
iia <kha2464@gmail.com>
2020-12-18 23:27:03 +0900

    [기능] 위치정보 조회 추가
    - 위치정보 의존성 추가
    - 위치 권한 요청(Permissions)
    - 사용자 위치정보 가져오기

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import {Alert} from 'react-native'
 import Loading from './Loading';
+import * as Location from 'expo-location';
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  }
+
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      //console.log(location);
+      const {
+        coords: { latitude, longitude }
+      } = location;
+      this.setState({ isLoading: false });
+    } catch (e) {
+      Alert.alert("위치를 찾지 못하였습니다.", "흑흑");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
   render() {
+    const {isLoading} = this.state;
     return <Loading/>;
   }
 
